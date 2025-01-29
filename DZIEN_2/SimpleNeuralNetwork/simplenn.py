73b4a374121d087ce8658d1a6a580538397bd1b0
albim72 <marcin.albiniak72@gmail.com>
2025-01-29 12:13:29 +0100

    Create simplenn.py

---
+++ DZIEN_2/SimpleNeuralNetwork/simplenn.py
@@ -0,0 +1,7 @@
+#tutaj tworzymy model naszej prostej sieci neuronowej
+import numpy as np
+
+class SimpleNeuralNetwork:
+    def __init__(self):
+        np.random.seed(1)
+        self.weights = 2*np.random.random((3,1))-1

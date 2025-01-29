33b8e1d9eac9637be99a79c01c82ca99e23caf87
albim72 <marcin.albiniak72@gmail.com>
2025-01-29 13:25:38 +0100

    Update simplenn.py

--- DZIEN_2/SimpleNeuralNetwork/simplenn.py
+++ DZIEN_2/SimpleNeuralNetwork/simplenn.py
@@ -12,3 +12,26 @@ class SimpleNeuralNetwork:
     def __repr__(self):
         return (f"nowa sieć neuronowa oparata na klasie: {self.__class__.__name__}\n"
                 f"Wylosowano wagi:\n{self.weights}\n")
+    
+    #funkcja aktywacji warstwy ukrytej
+    def sigmoid(self,x):
+        return 1/(1+np.exp(-x))
+    
+    #różniczka funkcji aktywacji
+    def d_sigmoid(self,x):
+        return x*(1-x)
+    
+    #funkcja propagacji
+    def propagation(self,inputs):
+        return self.sigmoid(np.dot(inputs.astype(float),self.weights))
+    
+    #funkcja propagacji wstecznej
+    def backward_propagation(self,propagation_result,train_input,train_output):
+        error = train_output - propagation_result
+        self.weights += np.dot(train_input.T,error*self.d_sigmoid(propagation_result))
+        
+    #funckja treningu sieci neuronowej
+    def train(self,train_input,train_output,train_iters):
+        for _ in range(train_iters):
+           propagation_result = self.propagation(train_input)
+           self.backward_propagation(propagation_result,train_input,train_output)

1d0cff0a6bac7e87928ff233fea6dfb6677a62aa
albim72 <marcin.albiniak72@gmail.com>
2025-01-29 13:39:28 +0100

    Update main.py

--- DZIEN_2/SimpleNeuralNetwork/main.py
+++ DZIEN_2/SimpleNeuralNetwork/main.py
@@ -4,3 +4,20 @@ from simplenn import SimpleNeuralNetwork
 network = SimpleNeuralNetwork()
 print(network)
 
+#zestw danych treningowych
+train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,0],[0,0,0],[0,0,1],[0,1,1]])
+#zestaw etykiet (odpowiedzi)
+train_outputs = np.array([[1,0,1,1,1,1,0,0]]).T
+#liczba epok
+train_iters = 50_000
+
+#trening sieci neuronowej
+network.train(train_inputs,train_outputs,train_iters)
+print(f"wytrenowane wagi:\n{network.weights}")
+
+#budowa zbioru testowego
+test_data = np.array([[1,1,1],[1,0,0],[0,1,1],[0,1,0],[0,0,1],[0,0,0]])
+
+print("__________ predykcja ___________")
+for data in test_data:
+    print(f"wynik dla {data} wynosi: {network.propagation(data)}")
